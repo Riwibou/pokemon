242819511d444612043fc3f829940d2fa17ec4f4
Riwan William Bououli <riwan.b@live.fr>
2024-04-23 16:37:38 +0200

    fixing the pokemons page

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -11,11 +11,10 @@ const Pokemons = () => {
       let allData = [];
       let nextUrl = "https://pokeapi.co/api/v2/pokemon";
 
-      // Boucle pour récupérer toutes les pages de résultats
       while (nextUrl) {
         const response = await axios.get(nextUrl);
         allData = [...allData, ...response.data.results];
-        nextUrl = response.data.next; // Lien vers la page suivante, ou null si c'est la dernière page
+        nextUrl = response.data.next;
       }
 
       setData(allData);
@@ -74,11 +73,61 @@ const PokemonTypes = ({ url }) => {
     fetchTypes();
   }, [url]);
 
+  const getTypeColor = (type) => {
+    switch (type) {
+      case 'fighting':
+        return '#FF8000'; // Fighting (Red)
+      case 'flying':
+        return '#81B9EF'; // Flying (Purple)
+      case 'poison':
+        return '#9141CB'; // Poison (Purple)
+      case 'ground':
+        return '#915121'; // Ground (Yellow)
+      case 'rock':
+        return '#AFA981'; // Rock (Brown)
+      case 'bug':
+        return '#91A119'; // Bug (Green)
+      case 'ghost':
+        return '#704170'; // Ghost (Indigo)
+      case 'steel':
+        return '#60A1B8'; // Steel (Silver)
+      case 'fire':
+        return '#E62829'; // Fire (Orange)
+      case 'water':
+        return '#6890F0'; // Water (Blue)
+      case 'grass':
+        return '#78C850'; // Grass (Green)
+      case 'electric':
+        return '#F8D030'; // Electric (Yellow)
+      case 'psychic':
+        return '#EF4179'; // Psychic (Pink)
+      case 'ice':
+        return '#3DCEF3'; // Ice (Cyan)
+      case 'dragon':
+        return '#5060E1'; // Dragon (Indigo)
+      case 'dark':
+        return '#624D4E'; // Dark (Brown)
+      case 'fairy':
+        return '#EF70EF'; // Fairy (Pink)
+      case 'unknown':
+        return '#68A090'; // Unknown (Gray)
+      case 'shadow':
+        return 'rgb(50, 50, 50)'; // Shadow (Brown)
+      case 'normal':
+        return '#9FA19F';
+      default:
+        return '#535BF2'; // Default color (Blue)
+    }
+  };
+
+
   return (
     <>
+    <div className={`card-footer ${types.length === 1 ? 'single-type' : ''}`}>
       {types.map((type, index) => (
-        <span key={index}>{type}</span>
+        <span key={index} style={{ backgroundColor: getTypeColor(type) }}>{type} </span>
       ))}
+    </div>
     </>
   );
 };
